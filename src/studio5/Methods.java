1e4791bd26bf124964e2fc6167ba048027d99d98
erickuo <erickuo@localhost>
2022-10-20 12:46:29 -0500

    Studio 5 done*

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,13 +16,13 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 		return distance;
 	}
 
 	/**
 	 * Draw a bull's eye at the given location with the given radius.
-	 *
+	 *a
 	 * @param x      the x coordinate of the center of the bull's eye
 	 * @param y      the y coordinate of the center of the bull's eye
 	 * @param radius the radius of the bull's eye
@@ -34,17 +34,23 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, 3*(radius)/4);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius/2);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
 
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius/4);
 		
 	}
 
@@ -75,7 +81,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
